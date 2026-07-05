a6fd2c85121da8cab703c824be24be8d9efd34dc
georgiy8 <georgiykupriyev@gmail.com>
2026-07-05 17:24:02 +0300

    Implement drag functionality for GUI elements

--- gui/services/drag.lua
+++ gui/services/drag.lua
@@ -1,1 +1,72 @@
+--========================================================--
+-- Pilgrammed GUI Library
+-- Drag Service
+--========================================================--
 
+local UserInputService = game:GetService("UserInputService")
+
+local Drag = {}
+
+------------------------------------------------------------
+-- Enable Drag
+------------------------------------------------------------
+
+function Drag.Enable(DragObject, Target)
+
+    local Dragging = false
+    local DragStart
+    local StartPosition
+
+    local function Update(Input)
+
+        local Delta = Input.Position - DragStart
+
+        Target.Position = UDim2.new(
+            StartPosition.X.Scale,
+            StartPosition.X.Offset + Delta.X,
+
+            StartPosition.Y.Scale,
+            StartPosition.Y.Offset + Delta.Y
+        )
+
+    end
+
+    DragObject.InputBegan:Connect(function(Input)
+
+        if Input.UserInputType ~= Enum.UserInputType.MouseButton1 then
+            return
+        end
+
+        Dragging = true
+        DragStart = Input.Position
+        StartPosition = Target.Position
+
+        Input.Changed:Connect(function()
+
+            if Input.UserInputState == Enum.UserInputState.End then
+                Dragging = false
+            end
+
+        end)
+
+    end)
+
+    UserInputService.InputChanged:Connect(function(Input)
+
+        if not Dragging then
+            return
+        end
+
+        if Input.UserInputType ~= Enum.UserInputType.MouseMovement then
+            return
+        end
+
+        Update(Input)
+
+    end)
+
+end
+
+------------------------------------------------------------
+
+return Drag
