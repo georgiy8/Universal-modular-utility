26da81a8a1110e75aacfe6a5237fc21da30a228f
georgiy8 <georgiykupriyev@gmail.com>
2026-07-08 16:27:23 +0300

    Remove auto-play sound on tab open
    Removed auto-play sound functionality when the tab is opened.

--- modules/Phantom-lancer.lua
+++ modules/Phantom-lancer.lua
@@ -3,21 +3,20 @@ return function(Window)
         Name = "Asset Test",
         Icon = "🖼️"
     })
-    
     local Section = Tab:CreateSection({
         Name = "PNG Test"
     })
-   
+    
     local path = "assets/Phantom-lancer/Images/phantom.png"
     local AssetId = getcustomasset(path)
-   
+    
     Section:AddLabel({
         Text = "Path: " .. path
     })
     Section:AddLabel({
         Text = "AssetId: " .. tostring(AssetId)
     })
-   
+    
     if AssetId then
         Section:AddImage({
             Image = AssetId,
@@ -28,20 +27,4 @@ return function(Window)
     else
         Section:AddLabel({ Text = "❌ getcustomasset returned nil" })
     end
-
-    --------------------------------------------------------
-    -- Auto Play Sound when Tab is opened
-    --------------------------------------------------------
-    local SoundPath = "assets/Phantom-lancer/Sounds/Plance_lasthit_03_ru.mp3"
-    
-    task.spawn(function()
-        task.wait(0.5) -- небольшая задержка
-        local sound = Instance.new("Sound")
-        sound.SoundId = getcustomasset(SoundPath)
-        sound.Volume = 0.7
-        sound.Parent = game:GetService("SoundService")
-        sound:Play()
-        
-        print("Auto played sound on tab open")
-    end)
 end
