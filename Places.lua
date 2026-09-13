b06025528af415db546e32c696700ea8385d2002
georgiy8 <georgiykupriyev@gmail.com>
2026-09-13 13:20:22 +0300

    Add structure for place modules in Places.lua

--- Places.lua
+++ Places.lua
@@ -1,1 +1,9 @@
-
+return {
+    [8651781069] = {
+        Repo = "https://raw.githubusercontent.com/USER/PLACE-MODULES/main/",
+        Modules = {
+            { Name = "", Order = 30 },
+            { Name = "", Order = 40 },
+        },
+    },
+}
