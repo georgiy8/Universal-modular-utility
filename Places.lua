64ea677a3e5f53ccca06c2936279104629ed1f41
georgiy8 <georgiykupriyev@gmail.com>
2026-09-13 15:20:39 +0300

    Fix repository URL casing in Places.lua

--- Places.lua
+++ Places.lua
@@ -1,7 +1,7 @@
 --Voxlblade--
 return {
     [8651781069] = {
-        Repo = "https://github.com/georgiy8/Voxlblade/tree/main/Modules/",
+        Repo = "https://github.com/georgiy8/Voxlblade/tree/main/modules/",
         Modules = {
             { Name = "Movement", Order = 30 },
             { Name = "Misc", Order = 40 },
